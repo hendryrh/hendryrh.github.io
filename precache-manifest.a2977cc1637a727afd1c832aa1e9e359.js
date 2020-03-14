f48116b020af1eb6e0122118d84a4e722f49a5ca
= <=>
2020-03-15 01:59:04 +0700

    re-initiate commit

---
+++ precache-manifest.a2977cc1637a727afd1c832aa1e9e359.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d97df3b68919dedf32d",
+    "url": "/css/chunk-vendors.bfbd45cb.css"
+  },
+  {
+    "revision": "02a3698aaa3d06797a38",
+    "url": "/css/navbarbefore.42d221d6.css"
+  },
+  {
+    "revision": "3c54a32c1a2d90e15633",
+    "url": "/css/viewAbout-vue.b334234b.css"
+  },
+  {
+    "revision": "e224da4ea5e3d123f99b",
+    "url": "/css/viewDashboard-vue.b334234b.css"
+  },
+  {
+    "revision": "60c039ef8554b7ff5d0824e87f4a8676",
+    "url": "/img/cat.60c039ef.png"
+  },
+  {
+    "revision": "77856a0e2d43b30d2475dc2b4960ceff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9362fc07d31fd50564",
+    "url": "/js/app.6e4e70fd.js"
+  },
+  {
+    "revision": "1d97df3b68919dedf32d",
+    "url": "/js/chunk-vendors.4e4c147b.js"
+  },
+  {
+    "revision": "02a3698aaa3d06797a38",
+    "url": "/js/navbarbefore.eb6c826f.js"
+  },
+  {
+    "revision": "3c54a32c1a2d90e15633",
+    "url": "/js/viewAbout-vue.dedef796.js"
+  },
+  {
+    "revision": "e224da4ea5e3d123f99b",
+    "url": "/js/viewDashboard-vue.54a644e4.js"
+  },
+  {
+    "revision": "d82657f869d471d54665b209d3d164bf",
+    "url": "/manifest.json"
+  }
+]);
