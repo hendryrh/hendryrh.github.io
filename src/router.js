af29523cab45afe0c62f11fbc74280438b338d10
= <=>
2020-03-15 01:15:50 +0700

    re initiate commit

--- src/router.js
+++ src/router.js
@@ -26,7 +26,7 @@ export default new Router({
       },
       {           
         name: 'About',           
-        path: '/About',           
+        path: '',           
         component: loadViewDashboard('About')         
       }            
       ]
